e13edced2a6fda911e97e48df1d7d01f0697469f
Niranjan Sendilkumar <niranjansendilkumar11@gmail.com>
2026-04-13 23:48:36 +0100

    Added Unit testing File

--- test_data.py
+++ test_data.py
@@ -9,7 +9,7 @@ from data import categorise_wind
 
 class TestWeatherFunctions(unittest.TestCase):
 
-    # 🌡️ Temperature Tests
+    #Temperature Tests
     def test_temperature_categories(self):
         self.assertEqual(categorise_temperature(-5), "Freezing")
         self.assertEqual(categorise_temperature(5), "Cold")
@@ -17,14 +17,14 @@ class TestWeatherFunctions(unittest.TestCase):
         self.assertEqual(categorise_temperature(16), "Mild")
         self.assertEqual(categorise_temperature(25), "Warm")
 
-    # 💨 Wind Tests
+    #Wind Tests
     def test_wind_categories(self):
         self.assertEqual(categorise_wind(2), "Calm")
         self.assertEqual(categorise_wind(5), "Breezy")
         self.assertEqual(categorise_wind(10), "Windy")
         self.assertEqual(categorise_wind(20), "Storm")
 
-    # 💧 Humidity Tests
+    #Humidity Tests
     def test_humidity_categories(self):
         self.assertEqual(categorise_humidity(30), "Dry")
         self.assertEqual(categorise_humidity(50), "Comfortable")
